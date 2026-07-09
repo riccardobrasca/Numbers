07e596e7ca4a6082775892327f6594cc3da7be74
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-07-09 14:07:07 +0800

    fix

--- Numbers/integers.lean
+++ Numbers/integers.lean
@@ -7,9 +7,6 @@ All the original work is by Kevin Buzzard.
 
 # The integers
 
-In this file we assume all standard facts about the naturals, and then build
-the integers from scratch.
-
 The strategy is to observe that every integer can be written as `a - b`
 for `a` and `b` naturals, so we define the "pre-integers" to be `MyNat × MyNat`, the pairs
 `(a, b)` of naturals. We define an equivalence relation `≈` on `MyNat × MyNat`, with the
